ae2962abbe7974dc0f3130288523059c4dd011b8
Maxim Mironenko <mmironenko@chanzuckerberg.com>
2019-01-16 10:22:21 +0600

    initial try on terraform module

---
+++ main.tf
@@ -0,0 +1,37 @@
+module "default_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  enabled    = "${var.enabled}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = "${var.attributes}"
+  tags       = "${var.tags}"
+}
+
+resource "aws_s3_bucket" "default" {
+  count         = "${var.enabled == "true" ? 1 : 0}"
+  bucket        = "${module.default_label.id}"
+  acl           = "${var.acl}"
+  region        = "${var.region}"
+  force_destroy = "${var.force_destroy}"
+  policy        = "${var.policy}"
+
+  versioning {
+    enabled = "${var.versioning_enabled}"
+  }
+
+  # https://docs.aws.amazon.com/AmazonS3/latest/dev/bucket-encryption.html
+  # https://www.terraform.io/docs/providers/aws/r/s3_bucket.html#enable-default-server-side-encryption
+  # TODO (Max): figure out how we can make it optional
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "${var.sse_algorithm}"
+        kms_master_key_id = "${var.kms_master_key_id}"
+      }
+    }
+  }
+
+  tags = "${module.default_label.tags}"
+}
---
+++ variables.tf
@@ -0,0 +1,79 @@
+variable "namespace" {
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+  type        = "string"
+}
+
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  type        = "string"
+}
+
+variable "name" {
+  description = "Name  (e.g. `app` or `db`)"
+  type        = "string"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+}
+
+variable "acl" {
+  description = "(Optional) The canned ACL to apply. We recommend log-delivery-write for compatibility with AWS services"
+  default     = "log-delivery-write"
+}
+
+variable "policy" {
+  description = "A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy."
+  default     = ""
+}
+
+variable "prefix" {
+  description = "(Optional) Key prefix. Used to manage object lifecycle events."
+  default     = ""
+}
+
+variable "region" {
+  description = "(Optional) If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee."
+  default     = ""
+}
+
+variable "force_destroy" {
+  description = "(Optional, Default:false ) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  default     = "false"
+}
+
+variable "versioning_enabled" {
+  description = "(Optional) A state of versioning. Versioning is a means of keeping multiple variants of an object in the same bucket."
+  default     = "false"
+}
+
+variable "sse_algorithm" {
+  description = "The server-side encryption algorithm to use. Valid values are AES256 and aws:kms"
+  default     = "AES256"
+}
+
+variable "kms_master_key_id" {
+  description = "The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms"
+  default     = ""
+}
+
+variable "enabled" {
+  description = "Set to `false` to prevent the module from creating any resources"
+  default     = "true"
+}
+
+# TODO: add vars from mail.tf

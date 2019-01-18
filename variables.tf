4f9cfcc33a774f061c53898b7d4e1de53756e134
Josh Myers <joshuajmyers@gmail.com>
2019-01-18 14:27:50 +0000

    Minor fixup of some variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "namespace" {
-  description = "Namespace (e.g. `eg` or `cp`)"
   type        = "string"
+  description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
   type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  description = "Name  (e.g. `app` or `db`)"
   type        = "string"
+  description = "Name  (e.g. `app` or `db`)"
 }
 
 variable "delimiter" {
@@ -28,58 +28,65 @@ variable "attributes" {
 variable "tags" {
   type        = "map"
   default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+  description = "Additional tags (e.g. `{ BusinessUnit = \"XYZ\" }`"
 }
 
 variable "acl" {
-  description = "The canned ACL to apply. We recommend log-delivery-write for compatibility with AWS services"
+  type        = "string"
   default     = "log-delivery-write"
+  description = "The canned ACL to apply. We recommend log-delivery-write for compatibility with AWS services"
 }
 
 variable "policy" {
-  description = "A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy."
+  type        = "string"
   default     = ""
+  description = "A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy."
 }
 
 variable "region" {
-  description = "If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee."
+  type        = "string"
   default     = ""
+  description = "If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee."
 }
 
 variable "force_destroy" {
-  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  type        = "string"
   default     = "false"
+  description = "A boolean string that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
 }
 
 variable "versioning_enabled" {
-  description = "A state of versioning. Versioning is a means of keeping multiple variants of an object in the same bucket."
+  type        = "string"
   default     = "false"
+  description = "A state of versioning. Versioning is a means of keeping multiple variants of an object in the same bucket."
 }
 
 variable "sse_algorithm" {
-  description = "The server-side encryption algorithm to use. Valid values are AES256 and aws:kms"
+  type        = "string"
   default     = "AES256"
+  description = "The server-side encryption algorithm to use. Valid values are `AES256` and `aws:kms`"
 }
 
 variable "kms_master_key_id" {
-  description = "The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms"
+  type        = "string"
   default     = ""
+  description = "The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms"
 }
 
 variable "enabled" {
+  type        = "string"
   description = "Set to `false` to prevent the module from creating any resources"
   default     = "true"
 }
 
-# User variables
-
 variable "user_enabled" {
-  description = "Set to `true` to create an S3 user with permission to access the bucket"
+  type        = "string"
   default     = "false"
+  description = "Set to `true` to create an S3 user with permission to access the bucket"
 }
 
 variable "allowed_bucket_actions" {
   type        = "list"
   default     = ["s3:PutObject", "s3:PutObjectAcl", "s3:GetObject", "s3:DeleteObject", "s3:ListBucket", "s3:ListBucketMultipartUploads", "s3:GetBucketLocation", "s3:AbortMultipartUpload"]
-  description = "List of actions to permit for S3 bucket"
+  description = "List of actions the user is permitted to perform on the S3 bucket"
 }

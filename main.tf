cf6662e5b73921ba9e6c46fc89190b56dc1d2ed4
Maxim Mironenko <mmironenko@chanzuckerberg.com>
2019-01-18 11:34:51 +0600

    bump terraform-aws-iam-s3-user version to 0.2.2

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket" "default" {
 }
 
 module "s3_user" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-iam-s3-user.git?ref=tags/0.2.1"
+  source       = "git::https://github.com/cloudposse/terraform-aws-iam-s3-user.git?ref=tags/0.2.2"
   namespace    = "${var.namespace}"
   stage        = "${var.stage}"
   name         = "${var.name}"

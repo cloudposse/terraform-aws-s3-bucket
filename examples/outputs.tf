0e20f2997420249618371455a2832967cd9cf15c
Josh Myers <joshuajmyers@gmail.com>
2019-01-21 14:02:40 +0000

    Mark outputs relating to AWS keys as sensitive

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -31,9 +31,11 @@ output "user_unique_id" {
 output "access_key_id" {
   value       = "${module.s3_bucket.access_key_id}"
   description = "The access key ID"
+  sensitive   = true
 }
 
 output "secret_access_key" {
   value       = "${module.s3_bucket.secret_access_key}"
   description = "The secret access key. This will be written to the state file in plain-text"
+  sensitive   = true
 }

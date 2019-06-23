f6473507d4d927a66a6d9b1d49571c30b511b49e
rverma-nikiai <rohit.verma@niki.ai>
2019-06-23 07:11:20 +0530

    referencing to rverma-nikiai for the time cloudposse updates didn't finished

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket" "default" {
 }
 
 module "s3_user" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-iam-s3-user.git?ref=master"
+  source       = "git::https://github.com/rverma-nikiai/terraform-aws-iam-s3-user.git?ref=master"
   namespace    = var.namespace
   stage        = var.stage
   name         = var.name

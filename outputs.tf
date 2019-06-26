e7d2643245c62d0aee52fc443ad3ec2cf7513271
aknysh <andriy.knysh@gmail.com>
2019-06-26 12:20:22 -0400

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "bucket_domain_name" {
-  value       = var.enabled == "true" ? join("", aws_s3_bucket.default.*.bucket_domain_name) : ""
+  value       = var.enabled ? join("", aws_s3_bucket.default.*.bucket_domain_name) : ""
   description = "FQDN of bucket"
 }
 
 output "bucket_id" {
-  value       = var.enabled == "true" ? join("", aws_s3_bucket.default.*.id) : ""
+  value       = var.enabled ? join("", aws_s3_bucket.default.*.id) : ""
   description = "Bucket Name (aka ID)"
 }
 
 output "bucket_arn" {
-  value       = var.enabled == "true" ? join("", aws_s3_bucket.default.*.arn) : ""
+  value       = var.enabled ? join("", aws_s3_bucket.default.*.arn) : ""
   description = "Bucket ARN"
 }
 

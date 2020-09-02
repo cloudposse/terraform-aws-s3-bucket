659909d37f94d4445b4695003c28238f28c94714
woz5999 <jeff.wozniak@umbralab.com>
2020-09-01 19:24:34 -0700

    use minimum versions for provider pinning #33

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.12.0, < 0.14.0"
 
   required_providers {
-    aws   = "~> 2.0"
-    local = "~> 1.2"
-    null  = "~> 2.0"
+    aws   = ">= 2.0"
+    local = ">= 1.2"
+    null  = ">= 2.0"
   }
 }

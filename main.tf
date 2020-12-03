2dc6cd4cffcbb0032ee1723d7ccd4cf617aa7f21
Jürgen Weber <jurgen@space-marine.org>
2020-12-03 11:14:34 +1100

    go back to mine

--- main.tf
+++ main.tf
@@ -150,7 +150,8 @@ resource "aws_s3_bucket" "default" {
 }
 
 module "s3_user" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-iam-s3-user.git?ref=0.11.0"
+  source       = "git::ssh://github.com/jurgenweber/terraform-aws-iam-s3-user.git?ref=master"
+  # source       = "git::https://github.com/cloudposse/terraform-aws-iam-s3-user.git?ref=0.11.0"
   enabled      = module.this.enabled && var.user_enabled ? true : false
   s3_actions   = var.allowed_bucket_actions
   s3_resources = ["${join("", aws_s3_bucket.default.*.arn)}/*", join("", aws_s3_bucket.default.*.arn)]

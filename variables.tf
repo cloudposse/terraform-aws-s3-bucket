87ec1894638f15b6f6fdb847c0c5ee149f8b98b7
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-04-07 11:34:38 +0000

    Auto Format

--- variables.tf
+++ variables.tf
@@ -258,12 +258,12 @@ variable "object_lock_configuration" {
     object_lock_enabled = string # Valid value is 'Enabled'
     rule = object({
       default_retention = object({
-        mode = string # Valid values are GOVERNANCE and COMPLIANCE.
-        days = number
+        mode  = string # Valid values are GOVERNANCE and COMPLIANCE.
+        days  = number
         years = number
       })
     })
   })
-  default = null
+  default     = null
   description = "A configuration of S3 object locking. With S3 Object Lock, you can store objects using a write-once-read-many (WORM) model. Object Lock can help prevent objects from being deleted or overwritten for a fixed amount of time or indefinitely."
 }

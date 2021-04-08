8f111408431bd9d6ba637fecfe14c2d83b28d631
Maxim Mironenko <maxim@cloudposse.com>
2021-04-08 16:08:44 +0300

    bridgecrew skip added for versioning check

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
 resource "aws_s3_bucket" "default" {
   #bridgecrew:skip=BC_AWS_S3_13:Skipping `Enable S3 Bucket Logging` check until bridgecrew will support dynamic blocks (https://github.com/bridgecrewio/checkov/issues/776).
   #bridgecrew:skip=CKV_AWS_52:Skipping `Ensure S3 bucket has MFA delete enabled` due to issue in terraform (https://github.com/hashicorp/terraform-provider-aws/issues/629).
+  #bridgecrew:skip=BC_AWS_S3_16:Skipping `Ensure all data stored in the S3 bucket have versioning enabled` due to bug with evaluation of variable default value.
   count         = module.this.enabled ? 1 : 0
   bucket        = local.bucket_name
   acl           = try(length(var.grants), 0) == 0 ? var.acl : null

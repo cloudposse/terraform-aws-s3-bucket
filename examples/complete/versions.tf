9e9a6fabcd0a5c36870406a4266f3d900a866b26
nitrocode <nitrocode@users.noreply.github.com>
2022-02-10 13:30:38 -0600

    Create versions.tf

---
+++ examples/complete/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.68.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.7"
+    }
+  }
+}

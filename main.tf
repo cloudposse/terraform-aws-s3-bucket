d7a49439ea48dfd91c06830fb5d9419329bd927d
John C. Bland II <johncblandii@users.noreply.github.com>
2023-08-03 16:39:07 -0500

    Fix `source_policy_documents` combined with `var.policy` being ignored (#201)
    * fix: source_policy_documents combined with var.policy is ignored
    * fix readme
    
    ---------
    
    Co-authored-by: Matt Calhoun <matt@cloudposse.com>

--- main.tf
+++ main.tf
@@ -459,7 +459,7 @@ data "aws_iam_policy_document" "aggregated_policy" {
 }
 
 resource "aws_s3_bucket_policy" "default" {
-  count      = local.enabled && (var.allow_ssl_requests_only || var.allow_encrypted_uploads_only || length(var.s3_replication_source_roles) > 0 || length(var.privileged_principal_arns) > 0 || length(var.source_policy_documents) > 0) ? 1 : 0
+  count      = local.enabled && (var.allow_ssl_requests_only || var.allow_encrypted_uploads_only || length(var.s3_replication_source_roles) > 0 || length(var.privileged_principal_arns) > 0 || length(local.source_policy_documents) > 0) ? 1 : 0
   bucket     = join("", aws_s3_bucket.default[*].id)
   policy     = join("", data.aws_iam_policy_document.aggregated_policy[*].json)
   depends_on = [aws_s3_bucket_public_access_block.default]

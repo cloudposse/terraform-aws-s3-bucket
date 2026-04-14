0696969109eb03f6b3353c0d0a5325ef1e564066
rankin-tr <43282033+rankin-tr@users.noreply.github.com>
2026-04-14 09:25:01 -0700

    fix: removes spacelift module-tests (#4)

--- module-tests/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.region
-
-  profile = "bai-mgmt-gbl-identity"
-  assume_role {
-    role_arn = "arn:aws:iam::983558388183:role/bai-mgmt-gbl-sandbox-terraform"
-  }
-}
